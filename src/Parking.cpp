78879f913b1025e30a84dfa05e8cda5ab0507cc5
Abdelaziz Sbaai <abdelazizsbaai@gmail.com>
2019-03-10 21:36:28 +0100

    ca marche mtn

--- src/Parking.cpp
+++ src/Parking.cpp
@@ -7,24 +7,21 @@ using namespace std;
 Parking::Parking(){
 	string s;
 	string fstline;
-	ifstream fichier("/home/chaoui/Bureau/L3/Lifap6/RushHour/Sujet/puzzle.txt",ios::in);
+	ifstream fichier("/home/sbaai/Bureau/GitProject/RushHour/Sujet/puzzle.txt",ios::in);
 	getline(fichier,fstline);
 	cout<<"la valuer de la premiere ligne"<<fstline<<endl;
 	int i = 0;
 	while(getline(fichier,s)){
 		cout<<"la valeur de la ligne en cour :"<<s<<endl;
-		/*voiture v={s[0],s[1],s[2],s[3]};
+		voiture v={s[0]-'0',s[1]-'0',s[2]-'0',s[3]-'0'};
 		cout<<v.ligne<<" "<<s[0]<<endl;
 		cout<<v.colonne<<" "<<s[1]<<endl;
 		cout<<v.longueur<<" "<<s[2]<<endl;
 		cout<<v.position<<" "<<s[3]<<endl;
-		//voitures[i].ligne=s[0];
-		//voitures[i].colonne=s[1];
-		//voitures[i].longueur=s[2];
-		//voitures[i].position=s[3];
-		voitures.push_back({s[0],s[1],s[2],s[3]});*/
+		voitures.push_back({s[0],s[1],s[2],s[3]});
 		i++;
 	}
+
 
 }
 
@@ -32,7 +29,7 @@ Parking::Parking(){
 void Parking::retournerligne(){
 	string fstline;
 	string s,s1;
-	ifstream fichier("/home/chaoui/Bureau/L3/Lifap6/RushHour/src/ff.txt");
+	ifstream fichier("/home/sbaai/Bureau/GitProject/RushHour/src/ff.txt");
 	if(fichier){cout<<"le fichier a été lu avec succes"<<endl;}
 	else{cout<<"ca a merder"<<endl;}
 	getline(fichier,fstline);
@@ -47,35 +44,30 @@ void Parking::retournerligne(){
 	cout<<" "<<v.ligne<<" "<<v.colonne<<" "<<v.longueur<<" "<<v.position<<endl;
 	voitures.push_back(v);
 }
-	
+
 
 void Parking::Afficher(){
-	/*string res;
-	int ** grille = new int[6][6];
+	string res;
+	int grille[6][6] = {{0}};
+
 	for(unsigned int i = 0 ; i < voitures.size() ; i++){
 		if(voitures[i].position == 0){//vertical
 			for(int j = 0 ; j < voitures[i].longueur ; j++){
-				grille[voitures[i].ligne][voitures[i].colonne+j] = i;
+				grille[voitures[i].ligne][voitures[i].colonne+j] = 1;
 			}
 		}
-		else if(voitures[i].position == 1){//horizental			
+		else if(voitures[i].position == 1){//horizental
 			for(int j = 0 ; j < voitures[i].longueur ; j++){
-				grille[voitures[i].ligne+j][voitures[i].colonne] = i;
+				grille[1][1] = 1;
 			}
 		}
 	}
-	res += "********";
-	for(int i=0;i < 6;i++){
-		for(int j=0;j < 6;j++){
-		res+="*";
-		if(grille[i][j] != null)
-			res += grille[i][j];
-		else
-			res += " ";
-		res+="*\n";
-		}
+
+	for(int i = 0 ; i < 6 ; i++){
+		for(int j = 0 ; j < 6 ; j++){
+			cout<<grille[i][j]<<' ';
+		}std::cout << endl;
 	}
-	res += "********\0";*/
 }
 
 Parking::~Parking(){}
--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,9 @@ using namespace std;
 
 int main() {
 
-	Parking p;
+	Parking p = Parking();
 	p.retournerligne();
-	//p.retournerligne();
-	/*for(unsigned int i=0 ; i < p.voitures.size();i++){
-		cout<<p.voitures[i].ligne<<p.voitures[i].colonne<<p.voitures[i].longueur<<p.voitures[i].position<<endl;
-	}
-	cout<<"  "<<p.voitures[0].ligne<<" "<<p.voitures[0].colonne<<" "<<p.voitures[0].longueur<<" "<<p.voitures[0].position<<endl;
-	voiture v={1,2,3,4};
-	cout<<v.ligne<<endl;	*/
-	cout<<"ta un main "<<endl;	
+	p.Afficher();
+
 	return 0 ;
 }
